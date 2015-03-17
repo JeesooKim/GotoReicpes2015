7aa1bf5df2b1b3344c58faf193fd2b0009f457cd
johnsonta87 <johnsonta87@gmail.com>
2015-03-17 14:56:09 -0400

    commit

--- Model/database.php
+++ Model/database.php
@@ -1,38 +1,32 @@
 <?php
 
-class Database {
-
-    private static $dsn = 'mysql:host=lotus.arvixe.com;dbname=gotorecipes_db';
-    private static $username = 'admin_gotorecipe';
-    private static $password = 'recipe';
-    //reference to db connection
-    private static $db;
-
-    private function __construct() {}
-
-    //return reference to pdo object
-    public static function getDB () {
-
-        if (!isset(self::$db)) {
-            try {
-                self::$db = new PDO(self::$dsn,
-                    self::$username,
-                    self::$password);
-                //echo "CONNECTED";
-            } catch (PDOException $e) {
-                $error_message = $e->getMessage();
-                include('../Errors/database_error.php');
-                exit();
-            }
-            
-        }
-        return self::$db;
-    }
+class Database{
+    //Database class to create a database connection object(Php Data Object)
     
+    private static $dsn='mysql:host=localhost;dbname=gotorecipe';
+    private static $username = 'root';
+    private static $password ='';
+    //reference to db connection
+    private static $dbCon;
+    
+public function __construct(){}
+    
+//return reference to pdo object
+public static function getDB(){
+    
+            if(!isset(self::$dbCon)){
+            //try to connec to db, alwasy use try and catch
+                    try{
+                        self::$dbCon = new PDO(self::$dsn, self::$username, self::$password);
+                        //echo "PDO connected";
+                    }
+                    catch(PDOException $e){
+                        $error_message= $e->getMessage();
+                        include('../Errors/database_error.php');
+                        exit();
+                    }
+            }  
+            return self::$dbCon;
+    }
 }
-
-//$pdo = Database::getDB();
-
 ?>
-
-
