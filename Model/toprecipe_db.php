9cf96d4763c7cccababc05c4f3e802ad0ee12de7
jadenjwlee <jaden.jwlee@gmail.com>
2015-03-19 05:47:24 -0400

    top recipe
    top recipe - public

---
+++ Model/toprecipe_db.php
@@ -0,0 +1,45 @@
+<?php
+class ToprecipeDB {
+	//use category and jobapplicants class
+	//four static method
+    public static function getRecipeCategory() {
+        $db = Database::getDB();
+        $query = 'select distinct dish_cat from recipes';
+        $result = $db->query($query);
+        $categories = array();
+        foreach ($result as $row) {
+            $categories[] = $row['dish_cat'];
+        }
+        return $categories;
+    }
+
+    public static function getTopRecipeByCategory($category) {
+        $db = Database::getDB();
+        if( $category == "" ){
+            $query = "select  (select count(cmt_id) from comments where dish_id = a.dish_id ) cnt, dish_id,"
+		."dish_name, dish_cat, dish_key, dish_num_serving, dish_cook_time from recipes a "
+		."order by cnt desc";
+        }else{
+            $query = "select  (select count(cmt_id) from comments where dish_id = a.dish_id ) cnt, dish_id,"
+		."dish_name, dish_cat, dish_key, dish_num_serving, dish_cook_time from recipes a "
+		." where dish_cat = '$category' order by cnt desc";
+        }
+         echo $query;   
+        $result = $db->query($query);
+        $toprecipe = array();
+        foreach ($result as $row) {
+            $toprecipe = new Toprecipe(
+                                   $row['cnt'],
+                                   $row['dish_id'],
+                                   $row['dish_name'],
+                                   $row['dish_cat'],
+                                   $row['dish_key'],
+                                   $row['dish_num_serving'],
+                                   $row['dish_cook_time']
+                    );
+            $toprecipes[] = $toprecipe;
+        }
+        return $toprecipes;
+    }
+}
+?>
---
+++ View/Public/TopRecipes/toprecipes.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Welcome to GoToRecipes</title>
+	<!--[if lt IE 9]>
+	      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	      <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+	<link href="../../../Content/css/bootstrap.css" rel="stylesheet">
+	<link href="../../../Content/css/style.css" rel="stylesheet" type="text/css">
+	<link href="../../../Content/css/reset.css" rel="stylesheet" type="text/css">
+	
+</head>
+
+<body>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../../../Content/js/bootstrap.min.js"></script>
+
+	<div id="container">
+		<?php include ('../../../View/Shared/header.php') ?>
+		<!--end top-->
+<?php
+require('../../../Model/database.php');
+require('../../../Model/toprecipe.php');
+require('../../../Model/toprecipe_db.php');
+
+$category_parm = "";
+if(isset($_POST['category'])){
+    $category_parm = $_POST['category'];
+}
+
+$categories = ToprecipeDB::getRecipeCategory();
+
+?>
+Today's Recipe
+                
+    <form action="toprecipes.php" method="post">
+        <select name="category">
+        <?php 
+        foreach ($categories as $category) : 
+            
+            if( strcmp($category_parm, $category) ){
+                echo "<option value='$category' selected >$category</option>";
+            }else{
+                echo "<option value='$category'  >$category</option>";
+            }
+        endforeach;
+        ?>
+        </select>
+
+        <input type="submit" name="submit" value="Submit" />
+    </form>                
+
+<?php
+
+$toprecipes = ToprecipeDB::getTopRecipeByCategory($category_parm);
+
+echo '<table border="1">';
+foreach ($toprecipes as $toprecipe) :
+    echo "<tr>";
+    echo "<td>";
+    echo $toprecipe->getCnt();
+    echo "</td>";
+    echo "<td>";
+    echo $toprecipe->getDishId();
+    echo "</td>";
+    echo "<td>";
+    echo $toprecipe->getDishName();
+    echo "</td>";
+    echo "<td>";
+    echo $toprecipe->getDishCat();
+    echo "</td>";
+    echo "<td>";
+    echo $toprecipe->getDishKey();
+    echo "</td>";
+    echo "<td>";
+    echo $toprecipe->getDishNumServing();
+    echo "</td>";
+    echo "<td>";
+    echo $toprecipe->getDishCookTime();
+    echo "</td>";
+    echo "</tr>";
+endforeach;
+
+?>
+                <?php include ('../../../View/Shared/footer.php') ?>
+	</div> <!--end container-->
+</body>
+</html>
