367e599b6706d3ddc77090c52aace14cd9149f2e
johnsonta87 <johnsonta87@gmail.com>
2015-04-10 11:53:09 -0400

    imageslider fix (working)

--- View/Shared/footer.php
+++ View/Shared/footer.php
@@ -1,15 +1,19 @@
-<footer class="footer">
-                <nav class="navbar navbar-default">
-                        <ul class="nav nav-pills">
-                                <li><a href="<?php  echo SERVERROOT.'/index.php';?>">Home</a></li>
-                                <li><a href="<?php echo PATH_PUBLIC_IMAGEGALLERY . '/imagegallery.php';?>">Image Gallery</a></li>
-                                <!--li><a href="#">FAQs</a></li-->
-                                <li><a href="<?php echo  PATH_PUBLIC_CONTACT . '/contactus_form.php';?>">Contact Us</a></li>
-                                <li><a href="<?php echo  PATH_PUBLIC_FAQ . '/faq.php';?>">FAQ</a></li>
-                                <li id="copyright">&copy; <?php echo date("Y"); ?> gotorecipes.com</li>
-                        </ul>
-                </nav>
-</footer> <!--end footer-->
-</div> <!-- end of #container-->
+
+<div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+    <div class="container-footer">
+        <div class="navbar-text pull-left">
+            <ul class="nav nav-pills">
+                        <li><a href="<?php  echo SERVERROOT.'/index.php';?>">Home</a></li>
+                        <li><a href="<?php echo PATH_PUBLIC_IMAGEGALLERY . '/imagegallery.php';?>">Image Gallery</a></li>
+                        <!--li><a href="#">FAQs</a></li-->
+                        <li><a href="<?php echo  PATH_PUBLIC_CONTACT . '/contactus_form.php';?>">Contact Us</a></li>
+                        <li><a href="<?php echo  PATH_PUBLIC_FAQ . '/faq.php';?>">FAQ</a></li>
+                        <li id="copyright">&copy; <?php echo date("Y"); ?> gotorecipes.com</li>
+                </ul>
+        </div>
+    </div>
+</div><!--full container close -->
+
+
 </body>
 </html>
