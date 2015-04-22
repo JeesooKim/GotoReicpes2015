546340a9fbc31ee0bdc91642884003abccf7475b
jadenjwlee <jaden.jwlee@gmail.com>
2015-04-22 15:12:24 -0400

    Volunteer - public
    Volunteer - public

--- View/Public/Volunteer/ref/header-iframe.php
+++ View/Public/Volunteer/ref/header-iframe.php
@@ -20,15 +20,15 @@
 	      <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 	<![endif]-->        
                     
-                    <link href="<?php echo  PATH_CSS; ?>/bootstrap.css" rel="stylesheet" />
-	<link href="<?php echo  PATH_CSS; ?>/style.css" rel="stylesheet" type="text/css" />
-	<link href="<?php echo  PATH_CSS; ?>/reset.css" rel="stylesheet" type="text/css" />
+                    <link href="../../../Content/js/bootstrap.css" rel="stylesheet" />
+	<link href="../../../Content/js/style.css" rel="stylesheet" type="text/css" />
+	<link href="../../../Content/js/reset.css" rel="stylesheet" type="text/css" />
 	
         
                 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
                 <!-- Include all compiled plugins (below), or include individual files as needed -->
-                    <script src="<?php echo PATH_JS; ?>/bootstrap.min.js"></script>                    
+                    <script src="../../../Content/js/bootstrap.min.js"></script>                    
 
                     <!-- The following is for Image Gallery -->                    
                     <link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
@@ -37,7 +37,7 @@
                     <script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
                     <script src="//blueimp.github.io/Gallery/js/blueimp-gallery.min.js"></script>
                     <script src="//blueimp.github.io/Gallery/js/bootstrap-image-gallery.js"></script>
-                    <script src="<?php echo PATH_JS; ?>/bootstrap-image-gallery.js"></script>
+                    <script src="../../../Content/js/bootstrap-image-gallery.js"></script>
                     <!-- Image Gallery CDN-->
 </head> 
 <body>
