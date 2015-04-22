7bfb409222ad17c638d53a07a4f4dc444524f3a6
JeesooKim <jeesoo.joa@gmail.com>
2015-04-22 15:48:20 -0400

    styles

--- index.php
+++ index.php
@@ -6,30 +6,31 @@
 
         <div id="content-recipes">
             <div class="col-md-4">
-                <div class="box1">
-                    <p>Top Recipes</p>
+                <div class="box">
+                    <a href="View/Public/Toprecipes/toprecipes.php"><img src="Content/uploads/images/button_1.jpg" alt="Top Recipes" /></a>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="box2">
-                    <p>Recipe Search</p>
+                <div class="box">
+                    <a href="View/Public/Searching/searching.php"><img src="Content/uploads/images/button_2.jpg" alt="Search Recipes" /></a>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="box3">
-                    <p>Events</p>
+                <div class="box">
+                    <a href="View/Public/Events/events.php"><img src="Content/uploads/images/button_3.jpg" alt="Events" /></a>
                 </div>
             </div>
             
-            <div class="col-md-4"> 
-                <h1 class="h1-title">Welcome</h1>
-                <h3>We love food and we love cooking. If you have a recipe you’d like to share, we’d love to hear from you!</h3>
-                <p>Looking for something delicious and dairy-free? You've come to the right place. Explore the recipes below, search for something specific or filter by category.
+            <div class="col-md-4">
+                <div clas="row">
+                <p class="SpecialFont">We love food and we love cooking. If you have a recipe you’d like to share, we’d love to hear from you!
+                Looking for something delicious and dairy-free? You've come to the right place. Explore the recipes below, search for something specific or filter by category.
                     We want people to enjoy their savoury love affair with cheese to its absolute fullest. We simply want to give people the joy of food recipes, without any compromise.</p>
-            </div>
+                </div>
+                </div>
             <div class="col-md-4"> 
                 <!--Image from: ca.daiyafoods.com/recipes -->
-                <img style="width:500px; margin: 30px;" src="./Content/uploads/images/top-mac_cheese-2-640.png" alt="" />
+                <img id="plate" src="./Content/uploads/images/top-mac_cheese-2-640.png" alt="Plate" />
             </div>
 
         
